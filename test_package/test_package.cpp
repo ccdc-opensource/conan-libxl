6feef043abe61a5f93a2ee6c86265ff6248d7a68
Claudio Bantaloukas <cbantaloukas@ccdc.cam.ac.uk>
2020-06-19 21:32:45 +0300

    use char in test

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -5,15 +5,9 @@ using namespace libxl;
 int main()
 {
     Book* book = xlCreateXMLBook();
-#ifdef _WIN32
-    Sheet* sheet = book->addSheet(L"my");
-    sheet->writeStr(5, 5, L"Hello, World !");
-    book->save(L"out.xlsx");
-#else
     Sheet* sheet = book->addSheet("my");
     sheet->writeStr(5, 5, "Hello, World !");
     book->save("out.xlsx");
-#endif
     book->release();
 
     return 0;
